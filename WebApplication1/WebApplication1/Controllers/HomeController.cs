2bec25c2c2bc98f8da356fe970c5c4a9901261d8
JSPPSP <32481703+JSPPSP@users.noreply.github.com>
2017-10-03 19:39:27 +0530

    meain ni pletina lena

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "bulleeet lena pops.";
 
             return View();
         }
